e46d337706927250062cc7a9045beb9d40447f78
Aleksey_N <nester.a.l@yandex.ru>
2023-06-05 22:01:25 +0300

    удалил мусор

--- OAuthService.MVC/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace OAuthService.MVC.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
